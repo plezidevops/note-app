cee181a7cb41fcda19e50d3d344dacd0ac4624bc
pascalmlaurent@gmail.com <plezidevops@gmail.com>
2022-07-17 16:55:29 -0400

    import and export mudules

---
+++ app.js
@@ -0,0 +1,7 @@
+const getNotes = require('./notes.js');
+
+console.log(getNotes());
+
+/* KEEPS for reference purposes */
+// const add = require('./utils.js');
+// console.log(add(2, 3));
---
+++ notes.js
@@ -0,0 +1,5 @@
+const getNotes = function () {
+  return 'Take the dog for a walk.';
+};
+
+module.exports = getNotes;
